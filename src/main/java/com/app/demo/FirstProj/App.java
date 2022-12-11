e5cffebdca1802aa9c5c05b50b0ec1dc5d6cd498
SamadMohammed <mohd.ssamad01@gmail.com>
2022-12-12 01:13:48 +0530

    Sending maven Proj to git

---
+++ src/main/java/com/app/demo/FirstProj/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.FirstProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Helloo Samad!!!" );
+    }
+}
